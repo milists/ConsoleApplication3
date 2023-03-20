df53b6ed4fabbe2038edc132c78d110407393c99
t.student <t.student@1010-09>
2023-03-20 18:46:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <time.h>
+#include <conio.h>
+#include<locale.h>
+#include <xlocmon>
+using namespace std;
+const int pole = 11;
+
+
+
+char field[pole][pole];
+
+
+void a() {
+	cout << "  ";
+	for (int i = 0; i < pole; i++) {
+		cout << i << " ";
+	}
+	cout << endl;
+	for (int i = 0; i < pole; i++) {
+		cout << i << " ";
+		for (int j = 0; j < pole; j++) {
+			cout << field[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+
+	a();
+}
